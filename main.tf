e0487dd4c59e83c031110587a07deebffeca2a58
gnamico <166223096+gnamico@users.noreply.github.com>
2024-05-02 11:01:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "gh-actions-build-monai-models-vpc"
-  }
-}
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
@@ -27,59 +23,24 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_security_group" "main" {
-  name        = "gh-actions-build-monai-models-sg"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform-deployer-key"
   public_key = file("/tmp/ssh_id_gh.pub")
 }
 
-resource "aws_network_interface" "main_nic" {
-  subnet_id       = aws_subnet.main.id
-  security_groups = [aws_security_group.main.id]
-  description     = "Main network interface for EC2 instance"
-  associate_public_ip_address = true # Correctly place the IP assignment here
-}
-
 resource "aws_instance" "vm" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.medium"
+  associate_public_ip_address = true
   key_name      = aws_key_pair.deployer.key_name
-
-  network_interface {
-    network_interface_id = aws_network_interface.main_nic.id
-    device_index         = 0
-  }
-
-  user_data = <<-EOF
-                #!/bin/bash
-                echo 'connected!'
-                EOF
 
   tags = {
     Name = "gh-actions-build-monai-models-vm"
   }
 }
 
-output "instance_public_ip" {
-  value = aws_network_interface.main_nic.association.public_ip
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.instance.public_ip
 }
 
