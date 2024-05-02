2f021a3844cf3c4f35d6cd1ced56ebea139b0578
gnamico <166223096+gnamico@users.noreply.github.com>
2024-05-02 10:51:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1" # Usando la región especificada
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
@@ -21,7 +21,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Asegúrate de que esta zona de disponibilidad sea válida
+  availability_zone = "us-east-1a"
   tags = {
     Name = "gh-actions-build-monai-models-subnet"
   }
@@ -49,23 +49,24 @@ resource "aws_security_group" "main" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform-deployer-key"
-  public_key = file("/tmp/ssh_id_gh.pub") //This file is in the vm where you run terraform!!
+  public_key = file("/tmp/ssh_id_gh.pub")
 }
 
 resource "aws_network_interface" "main_nic" {
   subnet_id       = aws_subnet.main.id
   security_groups = [aws_security_group.main.id]
+  description     = "Main network interface for EC2 instance"
+  associate_public_ip_address = true # Correctly place the IP assignment here
 }
 
 resource "aws_instance" "vm" {
-  ami           = "ami-04b70fa74e45c3917" # AMI especificado
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.medium"
   key_name      = aws_key_pair.deployer.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.main_nic.id
     device_index         = 0
-    associate_public_ip_address = true
   }
 
   user_data = <<-EOF
@@ -79,5 +80,6 @@ resource "aws_instance" "vm" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.vm.public_ip
+  value = aws_network_interface.main_nic.association.public_ip
 }
+
