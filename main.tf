f7fa5c89207b88b370c252d179ac8746cc1e7af2
gnamico <166223096+gnamico@users.noreply.github.com>
2024-05-02 11:09:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,6 @@ resource "aws_instance" "vm" {
 
 output "instance_ip" {
   description = "The public ip for ssh access"
-  value       = aws_instance.instance.public_ip
+  value       = aws_instance.vm.public_ip 
 }
 
