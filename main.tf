bff0b29ad3694ae6dfa2e3f9f32b1c629f1fb742
gnamico <166223096+gnamico@users.noreply.github.com>
2024-05-02 12:57:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["<tu dirección IP>/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
